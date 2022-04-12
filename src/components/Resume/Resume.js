7d55409fb9625e276c951e1c0cf4340b425da24d
Ezequiel carrizo <ezequielce490@gmail.com>
2022-04-12 15:04:09 -0300

    change link

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,12 +5,15 @@ import "./Resume.css";
 import curriculum from "../../assets/curriculum.pdf";
 import { Document, Page } from "react-pdf";
 
+const curriculumLink =
+  "https://raw.githubusercontent.com/Ezequiel-CE/Ezequiel-CE..github.io/ddca3c4d6538e948ca6e10b2890e2aad80017ea6/src/assets/curriculum.pdf";
+
 const Resume = () => {
   return (
     <>
       <Container fluid className="resume-section">
         <Row className="resume">
-          <Document file={curriculum}>
+          <Document file={curriculumLink}>
             <Page pageNumber={1} />
           </Document>
         </Row>
