1a1bd1ffcfc428191470f79bcc4bbba70ac3ed3a
Ezequiel carrizo <ezequielce490@gmail.com>
2022-04-12 15:56:09 -0300

    change added pdf on the page

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Button } from "react-bootstrap";
 import { AiOutlineDownload } from "react-icons/ai";
 import "./Resume.css";
 import curriculum from "../../assets/curriculum.pdf";
 import { Document, Page } from "react-pdf";
-
-const curriculumLink =
-  "https://raw.githubusercontent.com/Ezequiel-CE/Ezequiel-CE..github.io/ddca3c4d6538e948ca6e10b2890e2aad80017ea6/src/assets/curriculum.pdf";
+//fix for the pdf
+import { pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume = () => {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    const handleRezise = () => {
+      const windowWidth = window.innerWidth;
+      setWidth(windowWidth);
+    };
+    window.addEventListener("resize", handleRezise);
+
+    return () => {
+      window.removeEventListener("resize", handleRezise);
+    };
+  }, []);
+
   return (
-    <>
+    <div>
       <Container fluid className="resume-section">
         <Row className="resume">
-          <Document file={curriculumLink}>
-            <Page pageNumber={1} />
+          <Document file={curriculum} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
+
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
@@ -28,7 +43,7 @@ const Resume = () => {
           </Button>
         </Row>
       </Container>
-    </>
+    </div>
   );
 };
 
