5e6f016ad69364fd9b2cbc6434609161b4612814
Ezequiel carrizo <ezequielce490@gmail.com>
2022-04-17 00:15:20 -0300

    added preloader to fake a loading

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import NavBar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
@@ -6,21 +7,36 @@ import About from "./components/About/About";
 import Resume from "./components/Resume/Resume";
 import useScrollTop from "./hooks/use-ScrollToTop";
 import Footer from "./components/Footer";
+import Preloader from "./components/Preloader";
+import "./index.css";
 
 function App() {
+  const [load, setLoad] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoad(false);
+    }, 1800);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   useScrollTop();
 
   return (
-    <>
+    <div id={load ? "no-scroll" : "scroll"}>
+      <Preloader load={load} />
       <Particle />
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Preloader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Preloader.module.css";
+import spinner from "../assets/spinner.svg";
+
+const Preloader = ({ load }) => {
+  return (
+    <div id={load ? styles.preloader : styles.preloaderNone}>
+      <img className="spinner" src={spinner} alt="spinner" />
+    </div>
+  );
+};
+
+export default Preloader;
